38dc0830f61051e1dbdee5519524df524826a527
Ana Castilla <ana.castilla1998@gmail.com>
2020-11-15 11:45:30 +0100

    Revert "Proyecto anterior"
    This reverts commit 8ac47e2b2307ee5807d7fb8b09838fedc23fc9a0.

--- app/src/main/java/es/iessaladillo/pedrojoya/intents/data/local/model/Pokemon.kt
+++
@@ -1,9 +0,0 @@
-package es.iessaladillo.pedrojoya.intents.data.local.model
-
-// TODO: Define las propiedades de un pokemon
-class Pokemon(
-    val id: Long,
-    val name: Int,
-    val image: Int,
-    val power: Int
-)
